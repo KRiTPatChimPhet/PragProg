fccbe82b9e8aec542ebfff47e337f21a3a52c97e
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-25 14:56:25 +0700

    Input string

---
+++ Assignment9.js
@@ -0,0 +1,3 @@
+inputString = prompt("What is the input string?")
+let length = inputString.length;
+console.log("What is the input string?",inputString,"\n",inputString,"has",inputString.length,"characters.")
