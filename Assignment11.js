d94ae2ccffb1d9688023a37cb803b296acf250be
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-25 17:44:23 +0700

    Mad Libs

---
+++ Assignment11.js
@@ -0,0 +1,11 @@
+var Noun = prompt("Enter a noun")
+var Verb = prompt("Enter a verb")
+var Adjective = prompt("Enter a adjective")
+var Adverb = prompt("Enter a adverb")
+console.log("Enter a noun :",Noun,"\n",
+            "Enter a verb :",Verb,"\n",
+            "Enter a adjective :",Adjective,"\n",
+            "Enter a adverb :",Adverb,"\n",
+            "Do you",Verb,"your",Adjective,"",Noun,"",Adverb,"?",
+            "นี่เหละที่มันฮา!"
+            )
