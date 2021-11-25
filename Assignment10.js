9114b4ca8362c37d27c045a51c1b356c6435e3de
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-25 15:10:30 +0700

    Input aphorisms or sayings

---
+++ Assignment10.js
@@ -0,0 +1,3 @@
+inputText1 = prompt("คำคม หรือ คำพังเพย อะไร?") 
+inputText2 = prompt("ใครเป็นคนกล่าวไว้?") 
+console.log("คำคม หรือ คำพังเพย อะไร?",inputText1," \n \n ใครเป็นคนกล่าวไว้?",inputText2,"\n \n",inputText2,"กล่าวว่า",inputText1)
