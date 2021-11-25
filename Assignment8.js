c5e23459f69b0251b13141c5a3314fa338ec3b65
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-25 14:47:30 +0700

    Input name

--- Assignment8.js
+++ Assignment8.js
@@ -1,2 +1,2 @@
 inputName = prompt("what is your name","โปรดระบุชื่อ")
-console.log("What is your name ?",inputName,"\n Hello",inputName,"nice to meet you")
+console.log("What is your name ?",inputName,"\n Hello,",inputName,"nice to meet you")
