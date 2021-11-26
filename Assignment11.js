b8a1169e39c639f408e47d3dc30321435f44b254
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-26 16:22:59 +0700

    Mad Libs

--- Assignment11.js
+++ Assignment11.js
@@ -2,8 +2,8 @@ var Noun = prompt("Enter a noun")
 var Verb = prompt("Enter a verb")
 var Adjective = prompt("Enter a adjective")
 var Adverb = prompt("Enter a adverb")
-console.log("Enter a noun :",Noun,"\n",
-            "Enter a verb :",Verb,"\n",
+console.log("Enter a noun :", Noun ,"\n",
+            "Enter a verb :", Verb ,"\n",
             "Enter a adjective :",Adjective,"\n",
             "Enter a adverb :",Adverb,"\n",
             "Do you",Verb,"your",Adjective,"",Noun,"",Adverb,"?",
