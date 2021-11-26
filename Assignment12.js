07d7c3ea56db6d0df290a9cd0c17a2ac24121be0
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-26 15:35:18 +0700

    imple Math

--- Assignment12.js
+++ Assignment12.js
@@ -1,6 +1,8 @@
 var a = parseInt(prompt("What is the first number?"))
 var b = parseInt(prompt("What is the second number?"))
-console.log(" " + a + "+" + " " + b + " = " + " " +(a+b),"\n",
+console.log("What is the first number?",a,"\n",
+            "What is the second number?",b,"\n",
+            " " + a + "+" + " " + b + " = " + " " +(a+b),"\n",
             " " + a + "-" + " " + b + " = " + " " +(a-b),"\n",
             " " + a + "*" + " " + b + " = " + " " +(a*b),"\n",
             " " + a + "/" + " " + b + " = " + " " +(a/b),"\n",
