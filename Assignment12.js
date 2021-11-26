319d370cb62a447af1846e33a498823abd39587a
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-11-26 16:23:14 +0700

    Simple Math

--- Assignment12.js
+++ Assignment12.js
@@ -2,8 +2,8 @@ var a = parseInt(prompt("What is the first number?"))
 var b = parseInt(prompt("What is the second number?"))
 console.log("What is the first number?",a,"\n",
             "What is the second number?",b,"\n",
-            " " + a + "+" + " " + b + " = " + " " +(a+b),"\n",
-            " " + a + "-" + " " + b + " = " + " " +(a-b),"\n",
-            " " + a + "*" + " " + b + " = " + " " +(a*b),"\n",
-            " " + a + "/" + " " + b + " = " + " " +(a/b),"\n",
+            " " + a + " + " + " " + b + " = " + " " +(a+b),"\n",
+            " " + a + " - " + " " + b + " = " + " " +(a-b),"\n",
+            " " + a + " * " + " " + b + " = " + " " +(a*b),"\n",
+            " " + a + " / " + " " + b + " = " + " " +(a/b),"\n",
             )
