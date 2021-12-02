c8e38e87693ce9bbe2eb5b83f91e0557c2a44dfd
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-02 11:34:35 +0700

    Add output

--- Assignment21.js
+++ Assignment21.js
@@ -1,13 +1,15 @@
 let orderAmount = Number(prompt("What is the order amount?"));
+console.log("What is the order amount?",orderAmount);
 let theState = prompt("What is the state?");
+console.log("What is the state?",theState);
 let tax;
 let total;
 if (theState == "WI"){
     tax = orderAmount * (5.5/100);
     total = orderAmount + tax ;
-    console.log("The sub total is: ",orderAmount,"\n The tax is:",tax)
+    console.log("The sub total is: ",orderAmount,"\n The tax is:",tax);
 }
 if (theState == "MN"){
-    total = orderAmount
+    total = orderAmount;
 }
-console.log("The total is:",total)
+console.log("The total is:",total);
