169a717c414aaa60cbfddb13eeeb99b3a30941cb
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-02 17:08:29 +0700

    Check age

---
+++ Assignment23.js
@@ -0,0 +1,3 @@
+let age = Number(prompt("What is your age?"));
+let checkAge = (age <= 18) ? "คุณอายุไม่ถึงเกณฑ์ที่จะขับรถอย่างถูกกฎหมาย" : "คุณอายุมากพอที่จะขับรถอย่างถูกกฎหมาย" ;
+console.log(checkAge);
