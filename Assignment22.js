8c3c2cfec7f911cc2e8f433331c4d42cc44726d3
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-02 10:26:15 +0700

    Password verification

--- Assignment22.js
+++ Assignment22.js
@@ -1,19 +1,10 @@
 let name = prompt("What is your name?")
 let password = prompt("What is the password?")
 let confirmPassword = prompt("Please reconfirm your password.")
-let yes;
-let no;
-if (password == '') {
-    alert ("Please enter Password");
-}
-if (confirmPassword == '') {
-    alert ("Please enter Password");
-}
-else if (password != confirmPassword) { 
-    no = ("ฉันไม่รู้จักคุณ.") 
+
+if (password != confirmPassword) { 
+    console.log("ฉันไม่รู้จักคุณ.") 
 } 	
 else{ 
-	yes = ("ยินดีต้อนรับ!")  
+	console.log("ยินดีต้อนรับ!")  
 } 
-console.log("What is your name?",name,"\n",
-            "What is the password?",password,"\n",yes,no)   
