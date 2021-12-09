e2a36770fc59a4663a1e09274114055ba44ec039
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-09 10:42:53 +0700

    Edit calculator

--- Assignment27.js
+++ Assignment27.js
@@ -1,5 +1,5 @@
 let order = Number(prompt("What is the order amount?"));
-let state = prompt("What state do you live in?");
+let state;
 let taxState;
 let county;
 let taxEau;
@@ -11,30 +11,37 @@ let totalD;
 let taxIllinois;
 let totalIllinois;
 let total;
-if (state == "Wisconsin" ){
-     taxState = order *(5.5 / 100);
-     county = prompt("What county do you live in?");
-     if (county == "Eau"){
-          taxEau = order *(0.5 / 100);
-          taxE = (taxState + taxEau).toFixed(2);
-          totalE = order + taxE
-          console.log("The tax is:",taxE,"\n The total is:",totalE);
+if(order){
+     state = prompt("What state do you live in?");
+     if (state == "Wisconsin" ){
+          taxState = order*(5.5 / 100);
+          county = prompt("What county do you live in?");
+          if (county == "Eau"){
+               taxEau = order*(0.5 / 100);
+               taxE = (taxState + taxEau);
+               totalE = order+taxE;
+               console.log("The tax is:",(taxE).toFixed(2),"\n The total is:",totalE);
+          }
+          else {if (county == "Dunn");{
+               taxDunn = order*(0.4 / 100);
+               taxD = (taxState+taxDunn);
+               totalD = order+taxD;
+               console.log("The tax is:",(taxD).toFixed(2),"\n The total is:",totalD);
+          }
      }
-     else {if (county == "Dunn");{
-          taxDunn = order *(0.4 / 100);
-          taxD = (taxState + taxDunn).toFixed(2);
-          totalD = order + taxD;
-          console.log("The tax is:",taxD,"\n The total is:",totalD);
+     }
+     if (state == "Illinois"){
+               taxIllinois = order*(0.8 / 100);
+               totalIllinois = taxIllinois + order;
+               console.log("The tax is:",(taxIllinois).toFixed(2),"\n The total is:",totalIllinois);
+     }
+     else {if (state == "Los Angelis"){
+               total = order;
+               console.log("The total is:",(total).toFixed(2));
+     }
      }
 }
-}
-if (state == "Illinois"){
-          taxIllinois = order *(0.8 / 100);
-          totalIllinois = taxIllinois + order;
-          console.log("The tax is:",taxIllinois,"\n The total is:",totalIllinois);
-}
-else {if (state == "Los Angelis"){
-          total = order;
-          console.log("The total is:",total);
-}
+else{
+     window.alert("Please enter the number of new orders.");
+     location.reload();
 }
