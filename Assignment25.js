ca7e5d7d2b9d73def790762ce23451e0399e60f3
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-13 14:45:28 +0700

    Edit if else.

--- Assignment25.js
+++ Assignment25.js
@@ -4,12 +4,13 @@ let f;
 let celsius;
 let c;
 console.log("Your choice:" ,choice)
-if (choice == "C" && "c"){
+if (choice == "C" ||choice == "c"){
      let fahrenheit = Number(prompt( "Please enter the temperature in Fahrenheit:" ));
      c = (fahrenheit - 32) * 5 / 9;
      console.log(" Please enter the temperature in Fahrenheit:",fahrenheit,"\n The temperature in Celsius is",c);
 } 
-else (choice == "F" && "f")
+else {if (choice == "F" ||choice == "f"){
      celsius = Number(prompt( "Please enter the temperature in Celsius:" )); 
      f = (celsius * 9 / 5) + 32;
      console.log(" Please enter the temperature in Celsius:" , celsius,"\n The temperature in Fahrenheit is", f);
+}}
