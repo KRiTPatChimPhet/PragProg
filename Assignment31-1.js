6da8b264b27a0797230645aa53ab1141dde84b66
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-21 16:44:23 +0700

    Edit length.

--- Assignment31-1.js
+++ Assignment31-1.js
@@ -1,16 +1,17 @@
 function isAnagram(a, b) {
+    if (a.length != b.length) {
+         return false;
+    }
     stra = a.toLowerCase().split("").sort().join("")
     strb = b.toLowerCase().split("").sort().join("")
-    stra.length;
-    strb.length;
-    return Boolean(stra === strb); 
+    return Boolean(stra === strb);
 }
 let a = prompt("Enter the first string:");
 let b = prompt("Enter the second string:");
 let s = isAnagram(a, b)
-if(s == true){
+if (s == true) {
     console.log("\"" + a + "\" and \"" + b + "\" are anagrams");
 }
 else
     console.log("\"" + a + "\" and \"" + b + "\" are not anagrams");
-isAnagram(a, b)     
+isAnagram(a, b)    
