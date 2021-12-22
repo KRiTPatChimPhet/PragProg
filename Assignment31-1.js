96381d85f8cd016427de8c516cdd136b7df16063
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2021-12-22 15:25:10 +0700

    Edit built-in.

--- Assignment31-1.js
+++ Assignment31-1.js
@@ -2,8 +2,14 @@ function isAnagram(a, b) {
     if (a.length != b.length) {
          return false;
     }
-    stra = a.toLowerCase().split("").sort().join("")
-    strb = b.toLowerCase().split("").sort().join("")
+    stra = a.toLowerCase();
+    strb = b.toLowerCase();
+    aStra = stra.split("");
+    aStrb = strb.split("");
+    console.log(stra,strb);
+    console.log(aStra,aStrb);
+    // stra = a.toLowerCase().split("").sort().join("")
+    // strb = b.toLowerCase().split("").sort().join("")
     return Boolean(stra === strb);
 }
 let a = prompt("Enter the first string:");
