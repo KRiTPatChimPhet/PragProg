d1f160983d916cfe1122a402a40b978d6ee367e0
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2022-01-04 23:11:03 +0700

    Calculating your heart rate with Kavonen Formula.

---
+++ Assignment38.js
@@ -0,0 +1,16 @@
+let intensity = 0;
+let a = 55;
+do {
+    restingHR = Number(prompt("Resting Pulse :"));
+} while (restingHR == NaN || restingHR == 0);
+do {
+    age = Number(prompt("Age :"));
+} while (age == NaN || age == 0);
+console.log("Resting Pulse: " + restingHR + " Age: " + age + "\n"
+    + "Intensity | Rate\n "+"---------|------")
+do {
+    intensity = a / 100
+    targetHeartRate = (((220 - age) - restingHR) * intensity) + (restingHR * 1)
+    console.log(a + "% | " + Math.round(targetHeartRate) + " bpm")
+    a += 5
+} while (a < 96)
