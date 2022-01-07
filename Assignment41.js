af5bc45f2882269f1f6ffd45bc2e4ca9f7e7f811
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2022-01-07 09:25:01 +0700

    Edit method.

--- Assignment41.js
+++ Assignment41.js
@@ -4,8 +4,12 @@ for (let i = 0; i < aName.length; i++) {
     console.log(aName[i]);
 }
 let inputName = prompt("Enter an employee name to remove?");
-aName.pop(inputName);
-console.log("There are", aName.length, "employees:")
+console.log("Enter an employee name to remove: "+inputName);
+let n = aName.indexOf(inputName);
+if (n > -1) {
+    aName.splice(n, 1);
+  }
+console.log("\nThere are", aName.length, "employees:");
 for (let i = 0; i < aName.length; i++) {
     console.log(aName[i]);
 }
