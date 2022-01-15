40d4f62409d8c28489abe0ca91344f5bc05a9351
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2022-01-15 13:36:19 +0700

    Add check input data.

--- Assignment9-Special.js
+++ Assignment9-Special.js
@@ -1,10 +1,10 @@
-let inputString = prompt("What is the input string?");
-let length;
-if (inputString == ""){
-    window.alert("Please fill in the information.")
-    location.reload;
-}
-else
-    length = inputString.length;
-console.log("What is the input string?",inputString,
-            "\n",inputString,"has",inputString.length,"characters.");
+let inputString
+do {
+    inputString = prompt("What is the input string?");
+    if (inputString == ""){
+	window.alert("กรุณากรอกข้อมูล")
+    }
+} while (inputString == "");
+let length = inputString.length;
+console.log("What is the input string?", inputString,
+            "\n"+inputString, "has", inputString.length, "characters.");
