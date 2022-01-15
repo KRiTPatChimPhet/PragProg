c4b9c15a6ac50de1077d9621528990a37adc712c
KRiTPatChimPhet <94230808+KRiTPatChimPhet@users.noreply.github.com>
2022-01-15 13:20:50 +0700

    Delete variable.

--- Assignment9.js
+++ Assignment9.js
@@ -1,5 +1,4 @@
 let inputString = prompt("What is the input string?");
-let length;
 let length = inputString.length;
 console.log("What is the input string?",inputString,
             "\n",inputString,"has",inputString.length,"characters.");
